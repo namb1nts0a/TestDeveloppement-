cf685d744abb8859de5945028028ed12ec0f3231
namb1nts0a <nambitsoanjaratiana@gmail.com>
2024-09-20 18:47:43 +0300

    add 404 & 500 errors pages for redirection

--- TestDeDeveloppement/Login/views.py
+++ TestDeDeveloppement/Login/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django import template
 
 # Create your views here.
 def login(request):
     return render(request, "login.html")
+
+
+def pages(request):
+    context = {}
+    # All resource paths end in .html.
+    # Pick out the html file name from the url. And load that template.
+    try:
+        load_template = request.path.split('/')[-1]
+
+        if load_template == 'admin':
+            return HttpResponseRedirect(reverse('admin:index'))
+        context['segment'] = load_template
+
+        html_template = loader.get_template('home/' + load_template)
+        return HttpResponse(html_template.render(context, request))
+
+    except template.TemplateDoesNotExist:
+
+        html_template = loader.get_template('404.html')
+        return HttpResponse(html_template.render(context, request))
+
+    except:
+        html_template = loader.get_template('500.html')
+        return HttpResponse(html_template.render(context, request))
--- TestDeDeveloppement/TestDeDeveloppement/urls.py
+++ TestDeDeveloppement/TestDeDeveloppement/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from Login.views import pages
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Login.urls'))
+    path('',include('Login.urls')),
+    re_path(r'^.*\.*', pages, name='pages'),
 ]
+
+
